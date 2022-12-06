c31af6fc04b0f38a608ab2a1a8f058775528c4b8
kutman2007 <116934243+kutman2007@users.noreply.github.com>
2022-12-06 09:59:19 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let mom = document.getElementById('dad')
+let sad = document.getElementById('bad')
+const cat=() => {
+    mom.value = "В мир";
+    mom.innerHTML = "В мир";
+    sad.value = "Чудес!!!"
+    sad.innerHTML = "Чудес!!!"
+}
